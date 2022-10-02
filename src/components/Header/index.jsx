a292fa144b0a00690b657afc90ef18a9c839ef79
Paulo <pholiveira2020@gmail.com>
2022-10-02 17:09:52 -0300

    Primeiro commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <div className="h-20 bg-base-profile w-full">
+      <h1>Ola amigo</h1>
+    </div>
+  )
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Nunito', 'sans serif'],
+      },
+      colors: {
+        brand: '#3294F8',
+        'base-brand': '#040F1A',
+        'base-background': '#071422',
+        'base-profile': '#0B1B2B',
+        'base-post': '#112131',
+        'base-border': '#1C2F41',
+        'base-label': '#3A536B',
+        'base-span': '#7B96B2',
+        'base-text': '#AFC2D4',
+        'base-subtitle': '#C4D4E3',
+        'base-title': '#E7EDF4',
+      },
+    },
+  },
+  plugins: [],
+}
